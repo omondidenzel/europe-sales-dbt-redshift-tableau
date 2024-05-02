b846afd48c0391f5228660d8d93ff50ffc552507
DENZEL OMONDI <omondidenzel@DENZELs-MacBook-Pro.local>
2024-05-02 20:06:08 +0200

    git cache cleared

--- etl.py
+++ etl.py
@@ -9,32 +9,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_password = os.getenv('DB_PASSWORD')
 region_name = os.getenv('AWS_REGION_NAME')
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-print(aws_secret_access_key)
-
-# # create connection engine
-# username = 'admin'
-# password = 'R1m7Xs6#O1bnyhoP'
-# port = 5439
-# db = 'sales'
-
-# endpoint = 'postgresql://{}:{}@default-workgroup.276527400122.us-east-1.redshift-serverless.amazonaws.com:{}/{}'.format(username,password,port,db)
-# # print(endpoint)
-# conn = create_engine(endpoint)
-
-# # Read data set 
-# def read_dataset(data):
-#     df = pd.read_csv(data)
-#     print(conn)
-#     # push data to database
-#     df.to_sql('europe', conn, if_exists='replace', index=False)
-    
-# data = 'Europe Sales Records.csv'
-# read_dataset(data)
 
 def fileUpload(data):
     s3 = boto3.resource(
